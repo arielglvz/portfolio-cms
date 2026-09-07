3f526aac9ed04e903aa68600c0d7ee97ef35af7b
arielglvz <arielglvz@gmail.com>
2026-09-07 08:05:37 +0800

    feat: build projects section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,56 @@
 const Projects = () => {
+  const projects = [
+    {
+      title: "AiChieve",
+      description: "An AI-powered goal planning application.",
+      technologies: ["React", "Firebase", "OpenAI", "Tailwind"],
+    },
+    {
+      title: "TODO-AI",
+      description: "An AI-powered goal planning application.",
+      technologies: [
+        "React",
+        "Redux",
+        "AppWrite",
+        "Google Generative AI",
+        "Tailwind",
+      ],
+    },
+    {
+      title: "Apteum Website - Rework",
+      description: "Reworked the website from the original Apteum website.",
+      technologies: ["React", "Tailwind"],
+    },
+  ]
+
   return (
-    <div>
+    <section className="mx-auto max-w-6xl px-6 py-20">
       <h1>Projects</h1>
-    </div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, optio.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {projects.map((project) => {
+          const { technologies } = project
+          return (
+            <div key={project.title} className="border p-4 rounded-md">
+              <h2 className="font-bold">{project.title}</h2>
+              <p className="text-base">{project.description}</p>
+              <ul className="flex flex-wrap gap-2">
+                {technologies.map((technology) => (
+                  <li
+                    key={technology}
+                    className="text-xs border rounded-sm px-2 py-1"
+                  >
+                    {technology}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )
+        })}
+      </div>
+    </section >
   )
 }
 
